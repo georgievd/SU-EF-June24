7760a6f89ee49786e2d491ca0c607e828a4d3c52
Doncho Georgiev <46848927+georgievd@users.noreply.github.com>
2024-06-28 20:55:49 +0300

    Add MusicHub Solution

---
+++ MusicHub/MusicHub/Data/Configuration.cs
@@ -0,0 +1,8 @@
+﻿namespace MusicHub.Data
+{
+    public static class Configuration
+    {
+        public static string ConnectionString =
+            @"Server=(localdb)\MSSQLLocalDB;Integrated Security=True;Database=MusicHub;Trusted_Connection=True";
+    }
+}
